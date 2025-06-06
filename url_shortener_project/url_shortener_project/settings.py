3a951182f8ceab1a34fcfe6a7f9f381fa19aac36
Edmond Abraham <3dm0nd6@gmail.com>
2025-06-06 11:55:22 -0500

    changed path in settings.py

--- url_shortener_project/url_shortener_project/settings.py
+++ url_shortener_project/url_shortener_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shortener',
+    'url_shortener_project.shortener',
     'rest_framework',
     'corsheaders',
 ]
